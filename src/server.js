85526a0a60f764100909ecdaf1e89f4c6c1dd6dc
gomesrodolfo <rodolfogomes.dev@gmail.com>
2022-03-12 14:55:50 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const PORT = 8080
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send({message:"Test CRUD"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running at ${PORT}...`)
+})
